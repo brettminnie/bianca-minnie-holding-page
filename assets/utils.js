761c4eca9576d24a928eb6d6ea1e7b4f480d8620
Brett Minnie <github@bdb-studios.co.uk>
2015-06-15 16:35:33 +0100

    Cleaned up JS

--- assets/utils.js
+++ assets/utils.js
@@ -2,27 +2,26 @@ $(function() {
 
     function resizePlaceHolder() {
 
-        var viewPortParams = {
+        var $viewPortParams = {
             width:      $(window).width(),
             height:     $(window).height(),
-            marginx:    parseInt(($(window).width()/20)),
-            marginy:    parseInt(($(window).height()/20))
+            margin:     {
+                y:  parseInt($(window).height()/20)
+            }
         };
-
 
         var element = $('#bianca-minnie-background');
 
-        element.css('margin-top', viewPortParams.marginy);
+        element.css('margin-top', $viewPortParams.margin.y);
 
         element.css('width', function(){
-            return parseInt(($(window).width()/20)*18);
+            return parseInt(($viewPortParams.width/20)*18);
         });
 
         element.css('height', function(){
-            return parseInt(($(window).height()/20)*18);
+            return parseInt(($viewPortParams.height/20)*18);
         });
 
-        console.log (viewPortParams);
     }
 
     resizePlaceHolder();
